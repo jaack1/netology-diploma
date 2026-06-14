3c044b4dab86b1a8c123f26f9695133d7fc16dbe
handsomejack <you@example.com>
2026-06-14 18:52:17 +0000

    test

--- terra-bootstrap/providers.tf
+++ terra-bootstrap/providers.tf
@@ -27,9 +27,3 @@ provider "yandex" {
   folder_id                    = var.folder_id
   zone                         = var.default_zone
 }
-
-provider "kubernetes" {
-  host                   = yandex_kubernetes_cluster.regional_cluster.master[0].external_v4_endpoint
-  cluster_ca_certificate = yandex_kubernetes_cluster.regional_cluster.master[0].cluster_ca_certificate
-  token                  = data.yandex_client_config.client.iam_token
-}
